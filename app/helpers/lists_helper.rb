77541c945c8410275bd236dc0a1fbe4baccfe260
Alexandre Ferreira <alex.ferreira9915@gmail.com>
2022-11-18 15:47:18 +0000

    Aula 30  exercicio 2 -images multi update

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,16 @@
 module ListsHelper
+  def image_cloudinary(list)
+    if list.photo.attached?
+      cl_image_path list.photo.key, crop: :fill
+    else
+      image_path "placeholder.png"
+    end
+  end
+  def image_cloudinary_tag(list)
+    if list.photo.attached?
+      cl_image_tag list.photo.key
+    else
+      image_tag "placeholder.png"
+    end
+  end
 end
